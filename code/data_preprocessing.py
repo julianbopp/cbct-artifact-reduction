0e535442c356a980f63d038c36193c6897ce0700
julianbopp <julian.bopp@tutanota.com>
2024-10-03 13:20:23 +0200

    Add Axeo data processing
    Also add fine control for what data will be processed.

--- code/data_preprocessing.py
+++ code/data_preprocessing.py
@@ -45,14 +45,24 @@ if __name__ == "__main__":
 
     # filter folder for accuitomo data
     accuitomo_folders = [f for f in folders if "Accuitomo" in f]
-    # filter folder for planmeca data
-    planmeca_folders = [f for f in folders if "Planmeca" in f]
     # filter folder for axeo data
     axeo_folders = [f for f in folders if "Axeo" in f]
+    # filter folder for planmeca data
+    planmeca_folders = [f for f in folders if "Planmeca" in f]
+
+    # Define which data to process
+    accuitomo = False
+    axeo = True
+    planmeca = False
 
     # Process Accuitomo data
     # First get all tif file paths
     for folder in accuitomo_folders:
+        if not accuitomo:
+            print("Skipping Accuitomo data")
+            break
+        else:
+            print("Processing Accuitomo data")
         # Create folder in output directory
         os.makedirs(os.path.join(output_dir, folder), exist_ok=True)
         # Get list of files in folder
@@ -68,3 +78,32 @@ if __name__ == "__main__":
                 tif_file,
                 os.path.join(output_dir, os.path.basename(folder), f"{index}.nii.gz"),
             )
+
+    # Process Axeos data
+    # First get all tif file paths
+    for folder in axeo_folders:
+        if not axeo:
+            print("Skipping Axeos data")
+            break
+        else:
+            print("Processing Axeos data")
+        os.makedirs(os.path.join(output_dir, folder), exist_ok=True)
+        # Get list of files in folder
+        files = os.listdir(os.path.join(data_dir, folder))
+        # Join the data path with the file name from files and Capture.tif
+        tif_paths = [
+            os.path.join(data_dir, folder, f, "correctedRawData.tif") for f in files
+        ]
+
+        # Save all tif files as nifti
+        for index, tif_path in enumerate(tif_paths):
+
+            tif_file = load.load_img_stack(tif_path, 1, 1, 1)
+            save.to_nii(
+                tif_file,
+                os.path.join(output_dir, os.path.basename(folder), f"{index}.nii.gz"),
+            )
+
+    # Process Planmeca data
+
+
