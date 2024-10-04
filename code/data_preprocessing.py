ef43aa2afd7c20c6281166e321b4edf2cc00ef1e
julianbopp <julian.bopp@tutanota.com>
2024-10-04 18:42:15 +0200

    Add func that returns info about a scan

--- code/data_preprocessing.py
+++ code/data_preprocessing.py
@@ -6,6 +6,50 @@ import tarfile
 import os
 
 
+def get_scanner_from_num(num: int):
+    orig_num = num
+    scanner = ""
+    material = ""
+    implants = ""
+    fov = ""
+
+    if ((num - 1) // 20) % 4 == 0:
+        scanner = "axeos"
+    elif ((num - 1) // 20) % 4 == 1:
+        scanner = "accuitomo"
+    elif ((num - 1) // 20) % 4 == 2:
+        scanner = "planmeca"
+    elif ((num - 1) // 20) % 4 == 3:
+        scanner = "x800"
+
+    if num % 10 == 0:
+        material = ""
+        implants = "0"
+    elif (num % 10) % 3 == 1:
+        implants = "3"
+    elif (num % 10) % 3 == 2:
+        implants = "2"
+    elif (num % 10) % 3 == 0:
+        implants = "1"
+
+    if ((num - 1) % 10) // 3 == 0:
+        material = "ti"
+    elif ((num - 1) % 10) // 3 == 1:
+        material = "tizr"
+    elif ((num - 1) % 10) // 3 == 2:
+        material = "zr"
+
+    if ((num - 1) // 10) % 2 == 0:
+        fov = "small"
+    elif ((num - 1) // 10) % 2 == 1:
+        fov = "large"
+
+    print(
+        f"Number: {orig_num}, Scanner: {scanner}, Material: {material}, Implants: {implants}, FOV: {fov}"
+    )
+    # return orig_num scanner, material, implants
+
+
 # Process tif data
 def tif_to_nifti(input_path: str, output_path: str):
     # Load the data
@@ -52,8 +96,8 @@ if __name__ == "__main__":
 
     # Define which data to process
     accuitomo = False
-    axeo = True
-    planmeca = False
+    axeo = False
+    planmeca = True
 
     # Process Accuitomo data
     # First get all tif file paths
@@ -105,5 +149,3 @@ if __name__ == "__main__":
             )
 
     # Process Planmeca data
-
-
