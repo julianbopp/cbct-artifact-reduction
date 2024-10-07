a387f5ef7f39165b44b32225b17c848ebb55e61d
julianbopp <julian.bopp@tutanota.com>
2024-10-07 15:22:47 +0200

    Refactor useful functions into separate file

---
+++ code/utils.py
@@ -0,0 +1,63 @@
+import os
+import tarfile
+from brainglobe_utils.IO.image import load, save
+
+# Save project root directory
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+CODE_DIR = os.path.join(ROOT_DIR, "code")
+OUTPUT_DIR = os.path.join(ROOT_DIR, "output")
+
+
+def get_scanner_from_num(num: int):
+    orig_num = num
+    scanner = ""
+    material = ""
+    implants = ""
+    fov = ""
+
+    if ((num - 1) // 20) % 4 == 0:
+        scanner = "axeos"
+    elif ((num - 1) // 20) % 4 == 1:
+        scanner = "accuitomo"
+    elif ((num - 1) // 20) % 4 == 2:
+        scanner = "planmeca"
+    elif ((num - 1) // 20) % 4 == 3:
+        scanner = "x800"
+
+    if num % 10 == 0:
+        material = ""
+        implants = "0"
+    elif (num % 10) % 3 == 1:
+        implants = "3"
+    elif (num % 10) % 3 == 2:
+        implants = "2"
+    elif (num % 10) % 3 == 0:
+        implants = "1"
+
+    if ((num - 1) % 10) // 3 == 0:
+        material = "ti"
+    elif ((num - 1) % 10) // 3 == 1:
+        material = "tizr"
+    elif ((num - 1) % 10) // 3 == 2:
+        material = "zr"
+
+    if ((num - 1) // 10) % 2 == 0:
+        fov = "small"
+    elif ((num - 1) // 10) % 2 == 1:
+        fov = "large"
+
+    return orig_num, scanner, material, implants, fov
+
+
+def extract_tar_gz(tar_gz_path: str, output_path: str):
+    file = tarfile.open(tar_gz_path)
+    file.extractall(output_path)
+    file.close()
+
+
+def tif_to_nifti(input_path: str, output_path: str):
+    # Load the data
+    data = load.load_img_stack(input_path, 1, 1, 1)
+    # Save the data
+    save.to_nii(data, output_path)
