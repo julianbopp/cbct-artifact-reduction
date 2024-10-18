ab0788ee860b2e28f117c8fb67ff8d23964628b5
julianbopp <julian.bopp@tutanota.com>
2024-10-18 15:02:12 +0200

    Move files

--- src/cbct_artifact_reduction/preprocessing/lakefs_test.py
+++ src/cbct_artifact_reduction/preprocessing/lakefs_test.py
@@ -1,9 +1,11 @@
-from lakefs_spec import LakeFSFileSystem
+from lakefs.client import Client
 from utils import OUTPUT_DIR
 import os
 
-fs = LakeFSFileSystem()
+# import utils
+clt = Client(
+    host="https://dbe-lakefs.dbe.unibas.ch:8000",
+)
 
 remote_path = "lakefs://cbct-pig-jaws/processed_data/101_2_accuitomo_ti_3im_sf_r.nii.gz"
 local_path = os.path.join(OUTPUT_DIR, "lakefs_test.nii.gz")
-
--- src/cbct_artifact_reduction/preprocessing/test_nibabel.py
+++ src/cbct_artifact_reduction/preprocessing/test_nibabel.py
@@ -1,9 +1,9 @@
-from tarfile import data_filter
 import nibabel as nib
 import os
 import matplotlib.pyplot as plt
-import utils
 import numpy as np
+
+import cbct_artifact_reduction.processing.utils
 
 path = os.path.join(utils.OUTPUT_DIR, "data", "1.nii.gz")
 np_array = utils.nifti_to_numpy(path)
