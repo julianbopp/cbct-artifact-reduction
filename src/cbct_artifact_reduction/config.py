31c2ba9e4ddebcf2a5828a02bd87128080b6913d
julianbopp <julian.bopp@tutanota.com>
2024-11-01 18:49:45 +0100

    Add a config.py that loads variables from a config.yaml

---
+++ src/cbct_artifact_reduction/config.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+import yaml
+
+from cbct_artifact_reduction.utils import ROOT_DIR
+
+with open(f"{ROOT_DIR}/config.yaml", "r") as f:
+    config: dict[str, dict[str, Any]] = yaml.safe_load(f)
+
+
+LAKEFS_HOST: str = config["lakefs"]["host"]
+LAKEFS_USERNAME: str = config["lakefs"]["username"]
+LAKEFS_PASSWORD: str = config["lakefs"]["password"]
+LAKEFS_SSL_CA_CERT: str = config["lakefs"]["ssl_ca_cert"]
+LAKEFS_DATA_REPOSITORY: str = config["lakefs"]["data_repository"]
+LAKEFS_COMMIT: str = config["lakefs"]["commit"]
+LAKEFS_CACHE_PATH: str = config["lakefs"]["cache_path"]
+LAKEFS_VERIFY_SSL: bool = config["lakefs"]["verify_ssl"]
+
+f.close()
