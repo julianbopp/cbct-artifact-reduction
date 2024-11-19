4b7daa4c30405f9d72c1187254cf4be2f2332df6
julianbopp <julian.bopp@tutanota.com>
2024-11-19 10:10:33 +0100

    Create new lakefs tests

--- tests/cbct-artifact-reduction/test_lakefs.py
+++ tests/cbct-artifact-reduction/test_lakefs.py
@@ -1,3 +1,6 @@
+import hashlib
+import os
+
 import cbct_artifact_reduction.config as cfg
 import cbct_artifact_reduction.lakefs_own as lakefs_own
 
@@ -9,9 +12,53 @@ def test_lakefs_connection():
     print(files)
 
 
+def testCustomBoto3ClientListFolder():
+    client = lakefs_own.CustomBoto3Client(f"{cfg.LAKEFS_DATA_REPOSITORY}")
+    client.list_files_in_folder("test")
+
+
+def testCustomBoto3ClientGetFileNoCache(tmp_path):
+    client = lakefs_own.CustomBoto3Client(f"{cfg.LAKEFS_DATA_REPOSITORY}")
+    client.cache_path = tmp_path
+    local_name = (
+        hashlib.md5(
+            "processed_data/frames/256x256/100_0.nii.gz".encode("utf-8")
+        ).hexdigest()
+        + ".nii.gz"
+    )
+    client.get_file("processed_data/frames/256x256/100_0.nii.gz")
+
+    assert os.path.exists(tmp_path / local_name)
+    os.remove(tmp_path / local_name)
+
+
+def testCustomBoto3ClientGetFileFromCache(tmp_path):
+    filename = (
+        hashlib.md5(
+            "processed_data/frames/256x256/100_0.nii.gz".encode("utf-8")
+        ).hexdigest()
+        + ".nii.gz"
+    )
+    with open(tmp_path / filename, "w") as f:
+        f.write("test")
+        f.close()
+
+    client = lakefs_own.CustomBoto3Client(f"{cfg.LAKEFS_DATA_REPOSITORY}")
+    client.cache_path = tmp_path
+
+    local_path = client.get_file("processed_data/frames/256x256/100_0.nii.gz")
+
+    assert os.path.join(tmp_path, filename) == local_path
+
+
 def test_lakefs_download():
     pass
 
 
 def test_lakefs_caching():
     pass
+
+
+if __name__ == "__main__":
+    test_lakefs_connection()
+    testCustomBoto3ClientListFolder()
