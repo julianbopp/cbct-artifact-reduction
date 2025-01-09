a8f3a732e8b8b9d564f7b8e8c507fcd077b94dc6
julianbopp <julian.bopp@tutanota.com>
2025-01-09 17:14:38 +0100

    Write to csv without ^M at end of line

--- src/cbct_artifact_reduction/csvcreator.py
+++ src/cbct_artifact_reduction/csvcreator.py
@@ -15,7 +15,7 @@ def createSliceMaskCSV(slices: list[str], masks: list[str], csv_output_path: str
     ), "The number of slices and masks paths must be the same."
 
     rows = zip(slices, masks)
-    with open(csv_output_path, "w", newline="") as file:
+    with open(csv_output_path, "w") as file:
         writer = csv.writer(file)
         writer.writerow(["slice", "mask"])
         writer.writerows(rows)
