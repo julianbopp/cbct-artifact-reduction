fd96b34f2774d6bb905ab4b8b2410a8c202bbac3
julianbopp <julian.bopp@tutanota.com>
2025-01-24 20:46:11 +0100

    Reexport some files in __init__

--- src/cbct_artifact_reduction/__init__.py
+++ src/cbct_artifact_reduction/__init__.py
@@ -1,1 +1,5 @@
 package = ""
+
+from cbct_artifact_reduction import config as config
+from cbct_artifact_reduction import dataprocessing as dataprocessing
+from cbct_artifact_reduction import utils as utils
