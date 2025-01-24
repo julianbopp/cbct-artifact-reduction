ccbeaede5deaad66a8a9f2490f9b1c75f9c6edb1
Julian Bopp <julian.bopp@dbe-cia-sl19-01.dbe.unibas.ch>
2025-01-24 19:38:59 +0000

    Add a run script to start training or sampling

---
+++ run.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Run this from the cbct-artifact-reduction folder
+
+ARG=$!
+
+if [[ -z "$1" ]]; then
+    echo "Usage: ./run train|sample"
+    exit 1
+elif [[ "$1" == "sample" ]]; then
+    echo "Input: sample. OK."
+elif [[ "$1" == "train" ]]; then
+    echo "Input: train. OK."
+else
+    echo "Invalid Input. Leaving..."
+    exit 1
+fi
+
+# GENERAL VARIABLES
+LOG_DIR="~/logs"
+
+# MODEL FLAGS
+MODEL_FLAGS="--image_size 256 --num_channels 128 --class_cond False --num_res_blocks 2 --num_heads 1 --learn_sigma True --use_scale_shift_norm False --attention_resolutions 16"
+DIFFUSION_FLAGS="--diffusion_steps 1000 --noise_schedule linear --rescale_learned_sigmas False --rescale_timesteps False"
+TRAIN_FLAGS="--lr 1e-4 --batch_size 1"
+
+# TRAINING VARIABLES
+TRAIN_SCRIPT="src/cbct_artifact_reduction/scripts/train_test.py"
+
+# SAMPLING VARIABLES
+SAMPLE_SCRIPT="src/cbct_artifact_reduction/scripts/sample_test.py"
+MODEL_PATH="/home/julian.bopp/logs/model015000.pt"
+
+# PRINTING VARIABLES
+echo "Logging to: $LOG_DIR"
+
+echo "--------------------"
+echo "MODEL_FLAGS:"
+echo $MODEL_FLAGS
+echo "--------------------"
+echo "DIFFUSION_FLAGS:"
+echo $DIFFUSION_FLAGS
+echo "--------------------"
+echo "TRAIN_FLAGS"
+echo $MODEL_FLAGS
+echo "--------------------"
+
+if [[ "$1" == "train" ]]; then
+    echo "Starting training"
+    echo "Running $TRAIN_SCRIPT"
+    python $TRAIN_SCRIPT $TRAIN_FLAGS $MODEL_FLAGS $DIFFUSION_FLAGS
+
+elif [[ "$1" == "sample" ]]; then
+    echo "Starting sampling"
+    echo "Running $SAMPLE_SCRIPT"
+    python $SAMPLE_SCRIPT --model_path $MODEL_PATH $MODEL_FLAGS $DIFFUSION_FLAGS
+fi
