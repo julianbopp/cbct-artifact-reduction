e7f80d2237205b6920f1e29be9baca179d22da98
julianbopp <julian.bopp@tutanota.com>
2025-01-27 10:59:50 +0100

    Add save sample to sample folder

--- src/cbct_artifact_reduction/scripts/sample_test.py
+++ src/cbct_artifact_reduction/scripts/sample_test.py
@@ -4,6 +4,8 @@ from datetime import datetime
 
 import cbct_artifact_reduction.config as cfg
 import cbct_artifact_reduction.pigjawdataset as dataset
+import nibabel as nib
+import torch
 from cbct_artifact_reduction import lakefs_own
 from cbct_artifact_reduction.guided_diffusion import dist_util, logger
 from cbct_artifact_reduction.guided_diffusion.script_util import (
@@ -19,6 +21,8 @@ def main():
     args = create_argparser().parse_args()
     dist_util.setup_dist()
     logger.configure(os.path.expanduser("~/logs/"))
+
+    SAMPLE_DIR = os.path.join(os.path.expanduser("~/"), "samples")
 
     today = datetime.now()
     logger.log(f"SAMPLING {today}")
@@ -42,7 +46,7 @@ def main():
     data = iter(dataloader)
 
     model.load_state_dict(
-        dist_util.load_state_dict(args.model_path, map_location="cpu")
+        dist_util.load_state_dict(args.model_path, map_location="cuda:0")
     )
 
     num_samples = 10
@@ -64,7 +68,15 @@ def main():
         )
         if sample is not None:
             sample = sample.detach().cpu()
-            print(sample.shape)
+            mask = mask.detach().cpu()
+            ground_truth = ground_truth.detach().cpu()
+            sample = torch.cat([sample, ground_truth, mask], dim=0).numpy()
+            sample_nifti_object = nib.nifti1.Nifti1Image(sample, None)
+            nib.save(
+                sample_nifti_object, os.path.join(SAMPLE_DIR, f"sample_{i}.nii.gz")
+            )
+
+            print(f"Saved sample {i}")
 
     model.to(dist_util.dev())
     if args.use_fp16:
@@ -86,6 +98,8 @@ def create_argparser():
         use_ddim=False,
         model_path="",
         num_ensemble=1,
+        image_size=256,
+        class_cond=False,
     )
     defaults.update(model_and_diffusion_defaults())
     parser = argparse.ArgumentParser()
