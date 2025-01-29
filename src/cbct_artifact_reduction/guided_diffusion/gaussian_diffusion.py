a5f619815bcbbbde206a0c7b68fc943b49d95660
julianbopp <julian.bopp@tutanota.com>
2025-01-29 22:14:00 +0100

    Change sample loop to return x_start_pred

--- src/cbct_artifact_reduction/guided_diffusion/gaussian_diffusion.py
+++ src/cbct_artifact_reduction/guided_diffusion/gaussian_diffusion.py
@@ -632,7 +632,12 @@ class GaussianDiffusion:
                     model_kwargs=model_kwargs,
                 )
                 yield out
-                img = out["sample"]  # This is the reduced noise image and has 1 channel
+                if i in [0, 1, 2]:
+                    img = out["pred_xstart"]
+                else:
+                    img = out[
+                        "sample"
+                    ]  # This is the reduced noise image and has 1 channel
 
     def p_sample_loop_progressive(
         self,
