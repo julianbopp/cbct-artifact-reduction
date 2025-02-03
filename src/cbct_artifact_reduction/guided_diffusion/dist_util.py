3a086f7cf4f925c2a645cd76f777fefceee91da5
julianbopp <julian.bopp@tutanota.com>
2025-02-03 11:56:50 +0100

    Formatting

--- src/cbct_artifact_reduction/guided_diffusion/dist_util.py
+++ src/cbct_artifact_reduction/guided_diffusion/dist_util.py
@@ -7,9 +7,9 @@ import os
 import socket
 
 import blobfile as bf
-from mpi4py import MPI
 import torch as th
 import torch.distributed as dist
+from mpi4py import MPI
 
 # Change this to reflect your cluster layout.
 # The GPU for a given rank is (rank % GPUS_PER_NODE).
@@ -47,7 +47,7 @@ def dev():
     Get the device to use for torch.distributed.
     """
     if th.cuda.is_available():
-        return th.device(f"cuda")
+        return th.device("cuda")
     return th.device("cpu")
 
 
@@ -55,7 +55,7 @@ def load_state_dict(path, **kwargs):
     """
     Load a PyTorch file without redundant fetches across MPI ranks.
     """
-    chunk_size = 2 ** 30  # MPI has a relatively small size limit
+    chunk_size = 2**30  # MPI has a relatively small size limit
     if MPI.COMM_WORLD.Get_rank() == 0:
         with bf.BlobFile(path, "rb") as f:
             data = f.read()
