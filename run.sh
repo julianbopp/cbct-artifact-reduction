35549b7a3ab1292c14e7083f9970285498539214
julianbopp <julian.bopp@tutanota.com>
2025-02-27 21:41:51 +0100

    Update scripts with linting hints

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Run this from the cbct-artifact-reduction folder
 
-
 if [[ -z "$1" ]]; then
     echo "Usage: ./run train|sample|resume"
     exit 1
@@ -13,13 +12,12 @@ elif [[ "$1" == "resume" ]]; then
     echo "Input: resume. OK."
 else
     echo "Usage: ./run train|sample|resume"
-    exit 1
     echo "Invalid Input. Leaving..."
     exit 1
 fi
 
 # GENERAL VARIABLES
-LOG_DIR="~/logs"
+LOG_DIR="$HOME/logs"
 
 # MODEL FLAGS
 MODEL_FLAGS="--image_size 256 --num_channels 128 --class_cond False --num_res_blocks 2 --num_heads 1 --learn_sigma True --use_scale_shift_norm False --attention_resolutions 16"
@@ -38,26 +36,26 @@ echo "Logging to: $LOG_DIR"
 
 echo "--------------------"
 echo "MODEL_FLAGS:"
-echo $MODEL_FLAGS
+echo "$MODEL_FLAGS"
 echo "--------------------"
 echo "DIFFUSION_FLAGS:"
-echo $DIFFUSION_FLAGS
+echo "$DIFFUSION_FLAGS"
 echo "--------------------"
 echo "TRAIN_FLAGS:"
-echo $TRAIN_FLAGS
+echo "$TRAIN_FLAGS"
 echo "--------------------"
 
 if [[ "$1" == "train" ]]; then
     echo "Starting training"
     echo "Running $TRAIN_SCRIPT"
-    python $TRAIN_SCRIPT $TRAIN_FLAGS $MODEL_FLAGS $DIFFUSION_FLAGS
+    python "$TRAIN_SCRIPT" "$TRAIN_FLAGS" "$MODEL_FLAGS" "$DIFFUSION_FLAGS"
 elif [[ "$1" == "resume" ]]; then
     echo "Resume training"
     echo "Using model: $MODEL_PATH"
     echo "Running $TRAIN_SCRIPT"
-    python $TRAIN_SCRIPT --resume_checkpoint $MODEL_PATH $TRAIN_FLAGS $MODEL_FLAGS $DIFFUSION_FLAGS
+    python "$TRAIN_SCRIPT" --resume_checkpoint "$MODEL_PATH" "$TRAIN_FLAGS" "$MODEL_FLAGS" "$DIFFUSION_FLAGS"
 elif [[ "$1" == "sample" ]]; then
     echo "Starting sampling"
     echo "Running $SAMPLE_SCRIPT"
-    python $SAMPLE_SCRIPT --model_path $MODEL_PATH $MODEL_FLAGS $DIFFUSION_FLAGS
+    python "$SAMPLE_SCRIPT" --model_path "$MODEL_PATH" "$MODEL_FLAGS" "$DIFFUSION_FLAGS"
 fi
