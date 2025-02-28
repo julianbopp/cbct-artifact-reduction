cceb16c6ea10a9c9026cee268fd43d426e718cc1
julianbopp <julian.bopp@tutanota.com>
2025-02-28 19:21:47 +0000

    Add some flags to run script

--- run.sh
+++ run.sh
@@ -24,14 +24,16 @@ LOG_DIR="~/logs"
 # MODEL FLAGS
 MODEL_FLAGS="--image_size 256 --num_channels 128 --class_cond False --num_res_blocks 2 --num_heads 1 --learn_sigma True --use_scale_shift_norm False --attention_resolutions 16"
 DIFFUSION_FLAGS="--diffusion_steps 1000 --noise_schedule linear --rescale_learned_sigmas False --rescale_timesteps False"
-TRAIN_FLAGS="--lr 1e-4 --batch_size 1"
+TRAIN_FLAGS="--lr 1e-4 --batch_size 8"
 
 # TRAINING VARIABLES
 TRAIN_SCRIPT="src/cbct_artifact_reduction/scripts/train_test.py"
 
 # SAMPLING VARIABLES
 SAMPLE_SCRIPT="src/cbct_artifact_reduction/scripts/sample_test.py"
-MODEL_PATH="/home/julian.bopp/logs/model015000.pt"
+SAMPLE_FLAGS="--random_masks False"
+
+MODEL_PATH="/home/julian.bopp/logs/model095000.pt"
 
 # PRINTING VARIABLES
 echo "Logging to: $LOG_DIR"
@@ -46,6 +48,10 @@ echo "--------------------"
 echo "TRAIN_FLAGS:"
 echo $TRAIN_FLAGS
 echo "--------------------"
+echo "SAMPLE_FLAGS:"
+echo $SAMPLE_FLAGS
+echo "--------------------"
+echo "TRAIN_FLAGS:"
 
 if [[ "$1" == "train" ]]; then
     echo "Starting training"
